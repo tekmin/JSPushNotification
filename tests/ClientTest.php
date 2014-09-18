6fb2fac5ef6542f7bbabd737f030acef98291732
tekmin <tekmin.ewe@jobstreet.com>
2014-09-18 16:34:25 +0800

    Add test case for publishGroupMessage()

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -9,6 +9,10 @@ define('JSPN_CONFIG_PATH', __DIR__ . '/config.php');
 
 use JSPushNotification\Client;
 use JSPushNotification\Entity\JSPNResponse;
+use JSPushNotification\Entity\JSPNNotification;
+use JSPushNotification\Entity\JSPNAPNSNotification;
+use JSPushNotification\Entity\JSPNGCMNotification;
+use JSPushNotification\Entity\JSPNGroupNotification;
 
 class ClientTest extends PHPUnit_Framework_TestCase {
 
@@ -115,5 +119,44 @@ class ClientTest extends PHPUnit_Framework_TestCase {
             array(9716635, '39b7dc856002786c0de3a5577b3bb0a6936e120b52ad2b8cf827ac2612faaba1', 1),
             array(9716635, '12345', 0),
         );
+    }
+    
+    public function testPublishGroupMessage() {
+        $client = new Client();
+        $notification = new JSPNNotification('>=3.0.0');
+        $notification->setMessage('This is the message');
+        $notification->addData('addData', 'Data');
+        $notification->addDataArray(array(
+           'addDataArray' => 'Array' 
+        ));
+        $notification->addOptions('addOptions', 'Options');
+        $notification->addOptionsArray(array(
+            'addOptionsArray' => 'Array'
+        ));
+        
+        $notification2 = new JSPNNotification('>=3.0.0');
+        $notification2->setMessage('This is the message2');
+        $notification2->addData('addData', 'Data');
+        $notification2->addDataArray(array(
+           'addDataArray' => 'Array' 
+        ));
+        $notification2->addOptions('addOptions', 'Options');
+        $notification2->addOptionsArray(array(
+            'addOptionsArray' => 'Array'
+        ));
+        
+        $apnsNotification = new JSPNAPNSNotification();
+        $apnsNotification->addNotification($notification);
+        
+        $gcmNotification = new JSPNGCMNotification();
+        $gcmNotification->addNotification($notification2);
+        
+        $groupNotification = new JSPNGroupNotification();
+        $groupNotification->addPlatformNotification($apnsNotification);
+        $groupNotification->addPlatformNotification($gcmNotification);
+        
+        $response = $client->publishGroupMessage(9716635, $groupNotification);
+        $this->assertTrue($response instanceof JSPNResponse);
+        $this->assertEquals($response->status, 1);
     }
 }
